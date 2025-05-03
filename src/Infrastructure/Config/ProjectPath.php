3016751b6f74a9c62413c109cf217f803e27907d
Ilia Karachev <ik@al5.ru>
2025-05-03 11:59:48 +0300

    chore: comments

--- src/Infrastructure/Config/ProjectPath.php
+++ src/Infrastructure/Config/ProjectPath.php
@@ -6,17 +6,11 @@ namespace App\Infrastructure\Config;
 
 final class ProjectPath
 {
-    /**
-     * Get base path of the project.
-     */
     public static function getBasePath(): string
     {
         return \dirname(__DIR__, 3);
     }
 
-    /**
-     * Get config path.
-     */
     public static function getConfigPath(string $configFile = ''): string
     {
         $path = self::getBasePath() . '/config';
